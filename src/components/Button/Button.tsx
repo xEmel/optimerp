5a94f01c5ee4dd479c9a88c99e5182aed2515897
Kamil <tashanek@gmail.com>
2021-04-19 23:32:58 +0200

    Change Button Component styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components"
 
 export const Button = styled.button`
+  z-index: 99;
   border: none;
   color: white;
   border-radius: ${props => props.theme.borderRadius};
@@ -12,11 +13,7 @@ export const Button = styled.button`
   );
   font-size: 1.4rem;
   height: 50px;
-  min-width: 150px;
-  padding: 10px 40px;
+  width: 190px;
   text-transform: uppercase;
   cursor: pointer;
-  &:hover {
-    box-shadow: 2px 2px 2px #222222;
-  }
 `
