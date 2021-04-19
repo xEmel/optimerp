c7d32dd286dbd82fac01c58285629da0a8ac12af
Kamil <tashanek@gmail.com>
2021-04-19 23:35:18 +0200

    Add Section Title component

---
+++ src/components/Sections/SectionTitle.tsx
@@ -0,0 +1,48 @@
+import React from "react"
+import styled from "styled-components"
+
+const Title = styled.h2`
+  font-size: 2.3rem;
+  text-align: center;
+  position: relative;
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 10px;
+`
+
+const Divider = styled.div`
+  width: 15%;
+  min-width: 80px;
+  max-width: 35%;
+  height: 8px;
+  border-radius: 8px;
+  margin: 20px auto;
+  background: ${({ theme }) => theme.colors.secondary};
+  position: relative;
+  &::before,
+  &::after {
+    content: "";
+    position: absolute;
+    width: 8px;
+    height: 8px;
+    border-radius: 8px;
+    background: ${({ theme }) => theme.colors.secondary};
+  }
+  &::after {
+    right: -15px;
+  }
+  &::before {
+    left: -15px;
+  }
+`
+
+export const SectionTitle: React.FC = ({ children }) => (
+  <Wrapper>
+    <Divider />
+    <Title>{children}</Title>
+  </Wrapper>
+)
