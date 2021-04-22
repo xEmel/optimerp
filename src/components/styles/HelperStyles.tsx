43645801fc4ea597afa9f7d561f7148a27deab30
Kamil <tashanek@gmail.com>
2021-04-23 01:15:19 +0200

    Add text format helper component

---
+++ src/components/styles/HelperStyles.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+type FormattedSpanProps = {
+  bold?: boolean
+  color?: string
+  primary?: boolean
+  secondary?: boolean
+}
+
+export const FormattedSpan = styled.span<FormattedSpanProps>`
+  font-weight: ${({ bold }) => bold && "700"};
+  color: ${({ color, primary, secondary, theme }) =>
+    (primary && theme.colors.primary) ||
+    (secondary && theme.colors.secondary) ||
+    color};
+`
