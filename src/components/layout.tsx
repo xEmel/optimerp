762bcd50253fbac3cde1e888e09bd15860966427
Kamil <tashanek@gmail.com>
2021-04-26 00:51:49 +0200

    Remove paddings from layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -21,15 +21,7 @@ const Layout: React.FC = ({ children }) => {
     <ThemeProvider theme={theme}>
       <Nav />
       <Header />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-      </div>
+      <main>{children}</main>
     </ThemeProvider>
   )
 }
