b1e8319c9d5530ad4f318c912eaccbfc95ee1843
Kamil <tashanek@gmail.com>
2021-04-26 20:31:04 +0200

    Remove nav and header from layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -10,17 +10,13 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import "./styles/layout.css"
 import styled from "styled-components"
-import { Header } from "./Header/Header"
+
 import { ThemeProvider } from "styled-components"
 import { theme } from "../theme"
-import { SectionTitle } from "./Sections/SectionTitle"
-import { Nav } from "./Header/Nav/Nav"
 
 const Layout: React.FC = ({ children }) => {
   return (
     <ThemeProvider theme={theme}>
-      <Nav />
-      <Header />
       <main>{children}</main>
     </ThemeProvider>
   )
